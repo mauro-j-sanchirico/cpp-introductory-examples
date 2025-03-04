c2e91778a9a48c75b35e1bcb65ffcdc00ba4fb1c
Mauro J. Sanchirico III <156733573+mauro-j-sanchirico@users.noreply.github.com>
2025-03-04 01:10:28 +0000

    Add vector examples

---
+++ vectors/ex03_reverse_range/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ranges> // C++20
+#include <string_view>
+#include <vector>
+
+int main()
+{
+    std::vector<std::string_view> words{"Cat", "Dog", "Goldfish", "Hamster"}; // sorted in alphabetical order
+
+    for (const auto &word : std::views::reverse(words)) // create a reverse view
+    {
+        std::cout << word << ' ';
+    }
+
+    std::cout << '\n';
+
+    return 0;
+}
